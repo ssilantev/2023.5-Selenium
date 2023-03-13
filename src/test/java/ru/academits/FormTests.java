5d62bb9b68c2695d3ed17c94ae122742f7b1c2fc
ssilantev <un.realgar@yandex.ru>
2023-03-13 17:55:54 +0700

    Editing

--- src/test/java/ru/academits/FormTests.java
+++ src/test/java/ru/academits/FormTests.java
@@ -16,19 +16,20 @@ public class FormTests {
     public void defineDriver() {
 
         String browser = System.getProperty("browser");
+
         switch (browser) {
-            case "chrome" -> {
+            case "chrome":
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
-            }
-            case "edge" -> {
+                break;
+            case "edge":
                 WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
-            }
-            case "firefox" -> {
+                break;
+            case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
-            }
+                break;
         }
         driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
@@ -38,19 +39,36 @@ public class FormTests {
     public void assetUrl() {
         Assertions.assertEquals("https://demoqa.com/automation-practice-form", driver.getCurrentUrl());
     }
+
+    @BeforeEach
+    public void minimalDataTypeSet() {
+
+        driver.findElement(By.cssSelector("#firstName")).sendKeys(TestData.name);
+        driver.findElement(By.cssSelector("#lastName")).sendKeys(TestData.lastName);
+        driver.findElement(By.cssSelector("#userNumber")).sendKeys(TestData.phone);
+        driver.findElement(By.cssSelector("[for='gender-radio-1']")).click();
+    }
+
     @Test
-    public void minimal() {
+    public void minRequiresRegTest() {
+        driver.findElement(By.cssSelector("#submit")).isDisplayed();
+        driver.findElement(By.cssSelector("#submit")).click();
 
-        WebElement nameLabel = driver.findElement(By.cssSelector("#userName-label"));
-        String nameLabelText = nameLabel.getText();
-        Assertions.assertEquals("Name", nameLabelText);
+        driver.findElement(By.cssSelector("[role='dialog']")).isDisplayed();
+
+
+
+
 
     }
 
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 }
+
+
+//    @AfterEach
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//}
---
+++ src/test/java/ru/academits/TestData.java
@@ -0,0 +1,8 @@
+package ru.academits;
+
+public class TestData {
+		static String name = "Ivan";
+		static String lastName = "Ivanov";
+		static String phone = "1234567890";
+
+}
